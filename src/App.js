f12e1d6caaccd0b5ff4ebad5c33e3dc070e2bc82
helmar81 <helmar81@gmail.com>
2024-03-14 11:53:43 +0100

    update new 3 commit

--- src/App.js
+++ src/App.js
@@ -1,106 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from "react";
 import "./styles.css";
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       fullName: '',
-//       weight: '',
-//       height: '',
-//       bmi: '',
-//       message: '',
-//       optimalWeight: ''
-//     };
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.calculateBMI = this.calculateBMI.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(e) {
-//     this.setState({ [e.target.name]: e.target.value });
-//   }
-
-//   calculateBMI() {
-//     let heightSquared = ((this.state.height / 100) * this.state.height) / 100;
-//     let bmi = this.state.weight / heightSquared;
-//     let low = Math.round(18.5 * heightSquared);
-//     let high = Math.round(24.99 * heightSquared);
-//     let message = '';
-//     if (bmi >= 18.5 && bmi <= 24.99) {
-//       message = 'You are in a healthy weight range';
-//     } else if (bmi >= 25 && bmi <= 29.9) {
-//       message = 'You are overweight';
-//     } else if (bmi >= 30) {
-//       message = 'You are obese';
-//     } else if (bmi < 18.5) {
-//       message = 'You are under weight';
-//     }
-
-//     this.setState({ message: message });
-//     this.setState({
-//       optimalWeight:
-//         'Your suggested weight range is between ' + low + ' - ' + high
-//     });
-//     this.setState({ bmi: Math.round(bmi * 100) / 100 });
-//   }
-
-//   handleSubmit(e) {
-//     this.calculateBMI();
-//     e.preventDefault();
-//     // console.log(this.state);
-//   }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header" />
-//         <form onSubmit={this.handleSubmit}>
-//           <h2>BMI calculator</h2>
-//           <div>
-//             <label>Please enter your name</label>
-//             <input
-//               autoComplete="off"
-//               type="text"
-//               name="fullName"
-//               value={this.state.fullName}
-//               onChange={this.handleChange}
-//             />
-//           </div>
-//           <div>
-//             <label>Enter your height in cm</label>
-//             <input
-//               type="number"
-//               name="height"
-//               value={this.state.height}
-//               onChange={this.handleChange}
-//             />
-//           </div>
-
-//           <div>
-//             <label>Enter your weight in kg</label>
-//             <input
-//               type="number"
-//               name="weight"
-//               value={this.state.weight}
-//               onChange={this.handleChange}
-//             />
-//             <br />
-//           </div>
-//           <div>
-//             {/* <input type="submit" value="Submit" /> */}
-//             <button type="submit">submit</button>
-//             <h3> {this.state.message} </h3>
-//             <h3> {this.state.bmi} </h3>
-//           </div>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
 
 class App extends React.Component {
   constructor() {
@@ -152,6 +52,8 @@ class App extends React.Component {
       src="/GIFSouad.gif"
       alt="Souad"
     />
+
+
           <h2> BMI calculator </h2>
           <div>
          
@@ -177,13 +79,33 @@ class App extends React.Component {
           <h2> {this.state.bmi} </h2>
           <h2> {this.state.msg} </h2>
           <p>check your Body mass index</p>
-          <p>Weight loss can dramatically improve overall health, alleviate symptoms and lower risk of developing obesity related diseases such as cardiovascular disease, type 2 diabetes and more.</p>
+          <p class="text-lg leading-relaxed text-gray-300n justify-around">
+          Weight loss can dramatically improve overall health, alleviate symptoms and lower risk of developing obesity related diseases such as cardiovascular disease, type 2 diabetes and more
+      </p>
+
+          <footer class="flex flex-col items-center">
+    <div class="text-sm text-center ">
+      <a
+      
+      href="https://helmarbachle.web.app/" rel="noopener noreferrer"  target="_blank"  >
+     
+       Copyright © {new Date().getFullYear()} 
+    </a>
+    </div>
+    </footer>
+      
+
+    
         </form>
+
+       </div>
+
        
-      </div>
+
+      
     );
   }
 
-  
-}
+ 
+} 
 export default App;
